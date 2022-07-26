82308b894f9b33d4547767af122a76c0763ce601
konghaobin <konghaobin@qjdchina.com>
2022-07-26 10:37:04 +0800

    适配

--- responsive/vwvh适配.js
+++ responsive/vwvh适配.js
@@ -1,14 +1,16 @@
-(function (window, document){
-  const docEl = document.documentElement
+// (function (window, document){
+//   const docEl = document.documentElement
 
-  resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
-  // orientationchange事件在设备的纵横方向改变时触发。
+//   resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
+//   // orientationchange事件在设备的纵横方向改变时触发。
 
-  const recalc = () => {
-    const clientWidth = docEl.clientWidth
-    docEl.style.fontSize = (100 / (clientWidth * 2)) * 100 + 'vw'
-  }
+//   const recalc = () => {
+//     const clientWidth = docEl.clientWidth
+//     docEl.style.fontSize = (100 / (clientWidth * 2)) * 100 + 'vw'
+//   }
 
-  window.addEventListener(resizeEvt, recalc , false)
-  window.addEventListener('DOMContentLoaded', recalc , false)
-})(window, document)
+//   window.addEventListener(resizeEvt, recalc , false)
+//   window.addEventListener('DOMContentLoaded', recalc , false)
+// })(window, document)
+
+// 待定
